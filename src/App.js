5e93d2838818a027936051eec82afb65caf8b188
Jacob Stenson <jacob@nosnets.co.uk>
2019-10-01 16:47:25 +0100

    Created base React app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello there lol
+    </div>
+  );
+}
+
+export default App;
