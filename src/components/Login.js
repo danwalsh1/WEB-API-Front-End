fb667971759106eee0c63ccbd2998d6a790e8837
stensonj <stensonj@uni.coventry.ac.uk>
2019-10-05 16:10:30 +0100

    Modal created and contains username and password fields

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,17 +11,29 @@ class Login extends React.Component{
     }
 
     handleOk = e => {
+        console.log("ok")
         console.log(e)
         this.setState({
             visible: false,
         })
     }
 
+    handleSubmit = e => {
+        console.log("submit")
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err){
+                console.log("Values recieved: ",values);
+            }
+        })
+    }
+
     render() {
+        const { getFieldDecorator } = this.props.form
         return (
           <div>
             <Button type="primary" onClick={this.showModal}>
-              Login
+              Login here
             </Button>
             <Modal
               title="Login"
@@ -29,7 +41,33 @@ class Login extends React.Component{
               onOk={this.handleOk}
               onCancel={this.handleCancel}
             >
-              <p>do a form in here so the user can log in</p>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Item>
+                    {getFieldDecorator('username', { rules: [{ required: true, message: 'Please input your username!' }],})(
+                        <Input
+                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        placeholder="Username"
+                        />,
+                    )}
+                    </Form.Item>
+                    <Form.Item>
+                    {getFieldDecorator('password', {rules: [{ required: true, message: 'Please input your Password!' }],})(
+                        <Input
+                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        type="password"
+                        placeholder="Password"
+                        />,
+                    )}
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Log in
+                        </Button>
+                    </Form.Item>
+
+                </Form>
+
             </Modal>
           </div>
         );
