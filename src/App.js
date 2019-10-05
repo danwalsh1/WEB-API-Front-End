a91a1eaad39dcf4335e7903152af9eca112bb5ac
stensonj <stensonj@uni.coventry.ac.uk>
2019-10-05 13:30:31 +0100

    Created class and imported necessary librarys in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Form} from 'antd'
+import Login from './components/Login'
+
+const LoginForm = Form.create({name: 'login'})(Login)
 
 function App() {
   return (
     <div>
       Hello there lol
+      <LoginForm />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Modal, Button, Form, Icon, Input} from 'antd'
+
+class Login extends React.Component{
+    
+}
+
+export default Login
