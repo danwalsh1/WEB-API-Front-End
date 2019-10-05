3a020ec14b92ed8494d75da5cad0869a11409325
walshd9 <walshd9@coventry.ac.uk>
2019-10-05 03:10:04 +0100

    Created directory for components and basic structure for the sign up UI component

---
+++ src/components/SignUp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class SignUp extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const element = (
+            <div>
+            </div>
+        );
+
+        return element;
+    }
+}
+
+export default SignUp;
