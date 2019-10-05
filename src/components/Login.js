a3be2104f6f0ffce92f84300b7fed462bf089ae0
stensonj <stensonj@uni.coventry.ac.uk>
2019-10-05 14:12:15 +0100

    Created modal

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,39 @@ import React from 'react'
 import {Modal, Button, Form, Icon, Input} from 'antd'
 
 class Login extends React.Component{
-    
+    state = { visible: false}
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        })
+    }
+
+    handleOk = e => {
+        console.log(e)
+        this.setState({
+            visible: false,
+        })
+    }
+
+    render() {
+        return (
+          <div>
+            <Button type="primary" onClick={this.showModal}>
+              Login
+            </Button>
+            <Modal
+              title="Login"
+              visible={this.state.visible}
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+            >
+              <p>do a form in here so the user can log in</p>
+            </Modal>
+          </div>
+        );
+      }
+
 }
 
 export default Login
