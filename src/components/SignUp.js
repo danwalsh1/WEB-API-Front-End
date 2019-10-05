ac8aa90be1a598f302f7966a9496bea6360788a8
walshd9 <walshd9@coventry.ac.uk>
2019-10-05 03:21:50 +0100

    Added Modal to my signup component which will be used to have the sign up form as a floating layer

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import {Modal, Button} from 'antd';
 
 class SignUp extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            visible: false
+        };
+
+        this.showModal = this.showModal.bind(this);
+        this.handleOk = this.handleOk.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+    }
+
+    showModal(){
+        this.setState({visible: true});
+    }
+
+    handleOk(ev){
+        console.log(ev);
+        this.setState({visible: false});
+    }
+
+    handleCancel(ev){
+        console.log(ev);
+        this.setState({visible: false});
     }
 
     render(){
         const element = (
             <div>
+                <Button type="primary" onClick={this.showModal}>
+                    Sign Up
+                </Button>
+                <Modal title="Sign Up" visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel}>
+                    <p>Form will go here!</p>
+                </Modal>
             </div>
         );
 
