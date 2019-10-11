87022df55af0b5c58e8f9b09cadec59912bcad4a
Jacob Stenson <stensonj@coventry.ac.uk>
2019-10-11 17:46:32 +0100

    Gets the data from the form and enters it into the state object

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,50 @@
 import React from 'react'
 import {Modal, Button, Form, Icon, Input} from 'antd'
+//import { throwStatement } from '@babel/types';
 
 class Login extends React.Component{
-    state = { visible: false}
+    constructor(props){
+        super(props)
+        this.state = { visible: false, 
+            username: '',
+            password: ''};
+    }
 
-    showModal = () => {
+    showModal = () =>{
         this.setState({
             visible: true,
         })
     }
 
-    handleOk = e => {
-        console.log("ok")
-        console.log(e)
-        this.setState({
-            visible: false,
-        })
+    handleOk = event =>{
+        console.log("ok");
+        console.log(event);
+        console.log(this.state)
+        const loginData = {username: this.state.username, password: this.state.password}
+
+        // Validate fields and check to make sure neither are null.
+        if (!loginData.username){
+            console.log("Username should not be null.")
+        }else if (!loginData.password){
+            console.log("Password should not be null.")
+        }else{
+            this.setState({
+                visible: false,
+            })
+
+            fetch("localhost:8080/api/v1.0/login/signin", {
+                method: 'post',
+                body: JSON.stringify(loginData),
+                headers: { 'Content-Type': 'application/json'},
+            })
+            .then(res => res.json())
+            .then(json => console.log(json));
+        }
     }
 
-    handleSubmit = e => {
-        console.log("submit")
-        e.preventDefault();
+    handleSubmit = event =>{
+        console.log("submit");
+        event.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err){
                 console.log("Values recieved: ",values);
@@ -28,10 +52,18 @@ class Login extends React.Component{
         })
     }
 
-    handleCancel = e => {
+    handleCancel = event =>{
         this.setState({
             visible: false,
         })
+    }
+
+    handleUsername = event =>{
+        this.setState({username: event.target.value})
+    }
+
+    handlePassword = event => {
+        this.setState({password: event.target.value})
     }
 
     render() {
@@ -52,7 +84,8 @@ class Login extends React.Component{
                     <Form.Item>
                     {getFieldDecorator('username', { rules: [{ required: true, message: 'Please input your username!' }],})(
                         <Input
-                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }}/>}
+                        onChange={this.handleUsername}
                         placeholder="Username"
                         />,
                     )}
@@ -60,7 +93,8 @@ class Login extends React.Component{
                     <Form.Item>
                     {getFieldDecorator('password', {rules: [{ required: true, message: 'Please input your Password!' }],})(
                         <Input
-                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }}/>}
+                        onChange={this.handlePassword}
                         type="password"
                         placeholder="Password"
                         />,
