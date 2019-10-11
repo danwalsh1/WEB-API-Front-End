7ff4997da3156e6e75841a7ce60ec0e6e5d6959c
walshd9 <walshd9@coventry.ac.uk>
2019-10-11 19:37:30 +0100

    Added code to send the input data to the back end

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -16,7 +16,10 @@ class SignUp extends React.Component{
         super(props);
         this.state = {
             visible: false,
-            confirmDirty: false
+            confirmDirty: false,
+            username: '',
+            password: '',
+            confirmPassword: ''
         };
 
         this.showModal = this.showModal.bind(this);
@@ -26,6 +29,9 @@ class SignUp extends React.Component{
         this.handleConfirmBlur = this.handleConfirmBlur.bind(this);
         this.compareToSecondPassword = this.compareToSecondPassword.bind(this);
         this.compareToFirstPassword = this.compareToFirstPassword.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(this);
     }
 
     /* Modal Functions - Start */
@@ -35,11 +41,24 @@ class SignUp extends React.Component{
 
     handleOk(ev){
         console.log(ev);
-        this.setState({visible: false});
         // TODO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         /* WARNING - NOTE:
         *  After testing, this function is run when the ok button is pressed, not the "handleSubmit" function
         */
+       const signUpData = {username: this.state.username, password: this.state.password, confirmPassword: this.state.confirmPassword};
+
+       // Validate fields
+       // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+       if(signUpData.username.length > 0 && signUpData.password.length > 0){
+            if(signUpData.password === signUpData.confirmPassword){
+                fetch('localhost:8080/api/v1.0/signup/register', {
+                    method: 'post',
+                    body: JSON.stringify(signUpData),
+                    headers: {'Content-Type': 'application/json'}
+                }).then(res => res.json()).then(json => console.log(json));
+                this.setState({visible: false});
+            }
+       }
     }
 
     handleCancel(ev){
@@ -79,6 +98,18 @@ class SignUp extends React.Component{
         const {value} = ev.target;
         this.setState({confirmDirty: this.state.confirmDirty || !!value});
     }
+
+    handleUsernameChange(ev){
+        this.setState({username: ev.target.value});
+    }
+
+    handlePasswordChange(ev){
+        this.setState({password: ev.target.value});
+    }
+
+    handleConfirmPasswordChange(ev){
+        this.setState({confirmPassword: ev.target.value});
+    }
     /* Form Functions - End */
 
     render(){
@@ -104,14 +135,14 @@ class SignUp extends React.Component{
                         <Form.Item label="Username">
                             {getFieldDecorator('username', {rules: [{required: true, message: 'Input a username!'}]})(
                                 <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25'}} />}
-                                placeholder="Username"
+                                placeholder="Username" onChange={this.handleUsernameChange}
                                 />
                             )}
                         </Form.Item>
                         <Form.Item label="Password" hasFeedback>
                             {getFieldDecorator('password', {rules: [{required: true, message: 'Input a password!'}, {validator: this.compareToSecondPassword}]})(
                                 <Input.Password prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25'}} />}
-                                placeholder="Password"
+                                placeholder="Password" onChange={this.handlePasswordChange}
                                 />
                             )}
                         </Form.Item>
@@ -120,7 +151,7 @@ class SignUp extends React.Component{
                             It is, therefore, used to validate the two passwords when the user clicks off entering the second password */}
                             {getFieldDecorator('confirm', {rules: [{required: true, message: 'Confirm your password!'}, {validator: this.compareToFirstPassword}]})(
                                 <Input.Password prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25'}} />} onBlur={this.handleConfirmBlur}
-                                placeholder="Confirm Password"
+                                placeholder="Confirm Password" onChange={this.handleConfirmPasswordChange}
                                 />
                             )}
                         </Form.Item>
