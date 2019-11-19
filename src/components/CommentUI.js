a5d37ae75f63393475a25b0c36fd8a0eb54484dd
walshd9 <walshd9@coventry.ac.uk>
2019-11-19 21:56:54 +0000

    Comments can now be added by the UI

--- src/components/CommentUI.js
+++ src/components/CommentUI.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import {Button, Form, Icon, List, Card, Radio, Input, Comment} from 'antd';
+import {Button, Form, List, Input, Comment} from 'antd';
+
+const {TextArea} = Input;
+
+const Editor = ({onChange, onSubmit, submitting, value}) => (
+    <div>
+        <Form.Item>
+            <TextArea rows={4} onChange={onChange} value={value} />
+        </Form.Item>
+        <Form.Item>
+            <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
+                Add Comment
+            </Button>
+        </Form.Item>
+    </div>
+);
 
 class CommentUI extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             commentData: [],
+            submitting: false,
+            newComment: '',
             activityItemId: this.props.itemId,
             commentsRecieved: false
         };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     /* Component Functions - Start */
@@ -37,12 +57,46 @@ class CommentUI extends React.Component{
                 }
             );
         }
-      }
+    }
 
-      /* Component Functions - End */
+    /* Component Functions - End */
+
+    /* Form Functions - Start */
+
+    async handleSubmit(){
+        console.log("Starting to submit comment");
+        if(!this.state.newComment){
+            console.log("Failed to submit comment!");
+            return;
+        }
+
+        this.setState({submitting: true});
+
+        let aDay = new Date();
+        let today = aDay.getFullYear() + "-" + aDay.getMonth() + "-" + aDay.getDate() + " " + aDay.getHours() + ":" + aDay.getMinutes() + ":" + aDay.getSeconds();
+
+        console.log(`Today: ${today}`);
+        let sqlData = {userId: localStorage.getItem("userId"), activityId: this.state.activityItemId, allText: this.state.newComment, dateCreated: today, dateModified: today};
+
+        await fetch('http://localhost:8080/api/v1.0/comments/create', {
+            method: 'post',
+            body: JSON.stringify(sqlData),
+            headers: {'Content-Type': 'application/json', 'Authorization' : 'Basic ' + window.btoa(localStorage.getItem("username")+':'+localStorage.getItem("password"))}
+        }).then(res => console.log(res.status));
+        this.setState({submitting: false});
+        console.log("Finished comment submit");
+        window.location.reload();
+    }
+
+    handleChange(ev){
+        this.setState({newComment: ev.target.value});
+    }
+
+    /* Form Functions - End */
 
     render(){
-        let commentData = this.state.commentData;
+        //let commentData = this.state.commentData;
+        const {commentData, submitting, newComment} = this.state;
 
         const element = (
             <div>
@@ -51,6 +105,7 @@ class CommentUI extends React.Component{
                         <Comment author={item.userId} content={item.allText} />
                     </li>
                 )}/>
+                <Editor onChange={this.handleChange} onSubmit={this.handleSubmit} submitting={submitting} value={newComment} />
             </div>
         );
 
