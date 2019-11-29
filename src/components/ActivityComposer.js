2cdf805b9eeab408953f477a181fde42944d017a
Jacob Stenson <stensonj@coventry.ac.uk>
2019-11-29 16:57:17 +0000

    Completed ActivityComposer validation

--- src/components/ActivityComposer.js
+++ src/components/ActivityComposer.js
@@ -36,32 +36,32 @@ class ActivityComposer extends React.Component{
 
         // Validation --
 
-        var invalidLetters = /^[0-9a-zA-Z]+$/;
+        // Define valid characters
+        var validLetters = /^[0-9a-zA-Z]+$/;
         // Title
-        if (activityData.title.length > 20 || typeof activityData.title != "string" || !activityData.title.match(invalidLetters)){
+        if (activityData.title.length > 20 || typeof activityData.title != "string" || !activityData.title.match(validLetters)){
             window.alert("Title must be less than 20 characters, must be of type string and must be alphanumeric.");
             return;
         }
 
         // Description
-        if (activityData.description.length > 280 || typeof activityData.description != "string" || !activityData.description.match(invalidLetters)){
+        if (activityData.description.length > 280 || typeof activityData.description != "string" || !activityData.description.match(validLetters)){
             window.alert("Description must be less than 280 characters, must be of type string and must be alphanumeric.");
             return;
         }
 
         // URL
-        if (activityData.url.length > 200 || typeof activityData.url != "string" || !activityData.url.match(invalidLetters)){
+        if (activityData.url.length > 200 || typeof activityData.url != "string" || !activityData.url.match(validLetters)){
             window.alert("url must be less than 200 characters, must be of type string and must be alphanumeric.");
             return;
         }
 
         // Location
-        if (activityData.location.length > 200 || typeof activityData.location != "string" || !activityData.location.match(invalidLetters)){
-            window.alert("location must be less than 200 characters, must be of type string and must be alphanumeric.");
+        if (activityData.location.length > 20 || typeof activityData.location != "string" || !activityData.location.match(validLetters)){
+            window.alert("location must be less than 20 characters, must be of type string and must be alphanumeric.");
             return;
         }
 
-        
         // End of validation -- 
 
         // Validate form
